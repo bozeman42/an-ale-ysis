9a112f3e642f3b7e24d11280c55ec476eb58b1ed
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-11-17 18:16:16 -0600

    search box is receiving response from API

---
+++ server/routes/beer.router.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let router = express.Router();
+let request = require('request');
+let API_KEY = process.env.API_KEY;
+
+router.get('/search',function(req,res){
+  req.query.key = API_KEY;
+  request('https://api.brewerydb.com/v2/search',{qs: req.query},function(error,response,body){
+    res.send(response.body);
+  });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 var bodyParser = require('body-parser');
 
 var passport = require('./strategies/sql.localstrategy');
@@ -9,12 +10,13 @@ var sessionConfig = require('./modules/session.config');
 var indexRouter = require('./routes/index.router');
 var userRouter = require('./routes/user.router');
 var registerRouter = require('./routes/register.router');
+let beerRouter = require('./routes/beer.router');
 
 var port = process.env.PORT || 5000;
 
 // Body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve back static files
 app.use(express.static('./server/public'));
@@ -29,11 +31,13 @@ app.use(passport.session());
 // Routes
 app.use('/register', registerRouter);
 app.use('/user', userRouter);
+app.use('/beer',beerRouter);
 
 // Catch all bucket, must be last!
 app.use('/', indexRouter);
 
 // Listen //
-app.listen(port, function(){
-   console.log('Listening on port:', port);
+app.listen(port, function () {
+  console.log('Listening on port:', port);
+  console.log('DB name', process.env.DATABASE_NAME)
 });
