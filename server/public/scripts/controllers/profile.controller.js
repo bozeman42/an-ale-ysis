2f807716d4accce388ae139db2f269e1d18b7c26
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-11-26 17:11:30 -0600

    new kind of chart?

--- server/public/scripts/controllers/profile.controller.js
+++ server/public/scripts/controllers/profile.controller.js
@@ -7,70 +7,81 @@ myApp.controller('ProfileController', function (UserService, BeerService) {
   vm.data = BeerService.data;
 
   vm.radarOptions = {
-
     scale: {
       ticks: {
         beginAtZero: true,
-        min: -0.5,
+        min: 0,
         max: 5,
         stepSize: 1
       }
     },
   };
 
-  // vm.radarDataOptions = {
-  //   lineTension: 0.1
-  // };
+  console.log('chart defaults!', Chart.defaults.global);
 
-  bs.getCategoryRatings();
+  Chart.defaults.global.legend;
+
+  vm.radarDataOptions = {
+    fillColor: "rgba(220,220,220,0.2)",
+    strokeColor: "rgba(220,220,220,1)",
+    pointColor: "rgba(220,220,220,1)",
+  };
+
+  bs.getCategoryRatings()
+  .then(() => {
+    
+  });
+
+
+  let canvas = document.getElementById("myChart");
+  let ctx = canvas.getContext('2d');
+  vm.myChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+      labels: ["IBU", "Rating"],
+      datasets: [{
+        label: 'Rating by IBU',
+        data: vm.data.ibuRatings,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(0,0,0,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }],
+        xAxes: [{
+          ticks: {
+            beginAtZero: true,
+          }
+        }]
+      }
+    }
+  });
 
   bs.getIbuRatings()
     .then(() => {
-      let ctx = document.getElementById("myChart");
-      let myChart = new Chart(ctx, {
-        type: 'scatter',
-        data: {
-          labels: ["IBU", "Rating"],
-          datasets: [{
-            label: 'Rating by IBU',
-            data: vm.data.ibuRatings,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(0,0,0,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }],
-            xAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
-            }]
-          }
-        }
-      });
-      console.log(vm.data.ibuRatings)
-      myChart.update();
+      console.log('chart data',vm.myChart);
+      console.log('ibu ratings',vm.data.ibuRatings)
+      vm.myChart.update();
+      console.log('ibu ratings',vm.data.ibuRatings)
     });
-
-
 });
