609fb95fe8311a1627c740f3e30b8b1293cc52fd
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-11-29 22:20:03 -0600

    Standardized indentation

--- server/modules/encryption.js
+++ server/modules/encryption.js
@@ -3,15 +3,15 @@ var bcrypt = require('bcrypt');
 var SALT_WORK_FACTOR = 10;
 
 var publicAPI = {
-  encryptPassword: function(password) {
-      var salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
-      return bcrypt.hashSync(password, salt);
+  encryptPassword: function (password) {
+    var salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
+    return bcrypt.hashSync(password, salt);
   },
-  comparePassword: function(candidatePassword, storedPassword) {
-      console.log('comparing passwords');
-      console.log(candidatePassword, storedPassword);
-      //ndidatePassword, this.password
-      return bcrypt.compareSync(candidatePassword, storedPassword);
+  comparePassword: function (candidatePassword, storedPassword) {
+    console.log('comparing passwords');
+    console.log(candidatePassword, storedPassword);
+    //ndidatePassword, this.password
+    return bcrypt.compareSync(candidatePassword, storedPassword);
   }
 };
 
